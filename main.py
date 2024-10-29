1da52f2aabeeca5a18bbf57e71e4ae957a0f6cc1
Nikolay Andreev <bakasaru@list.ru>
2024-10-29 18:56:21 +0300

    feat: add command for 10 last updated games

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# import requests
 #
-# url = 'https://search.nintendo-europe.com/en/select?fq=type%3AGAME+AND+system_type%3Anintendoswitch*+AND+product_code_txt%3A*&q=*&sort=sorting_title+asc&start=0&wt=json&rows=2'
-# data = requests.get(url).json()
+
 #
 # print(data['response']['docs'][0]['title'])
 # print(data['response']['docs'][1]['title'])
 
 import telebot
 from dotenv import load_dotenv
+import requests
 from os import getenv
 
 load_dotenv()
@@ -19,6 +18,14 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(commands=['games'])
+def send_games(message):
+    url = 'https://search.nintendo-europe.com/en/select?fq=type%3AGAME+AND+system_type%3Anintendoswitch*+AND+product_code_txt%3A*&q=*&sort=change_date+desc&start=0&wt=json&rows=10'
+    data = requests.get(url).json()
+    response = '\n'.join([game['title'] for game in data['response']['docs']])
+    bot.send_message(message.chat.id, response)
 
 
 @bot.message_handler(func=lambda m: True)
