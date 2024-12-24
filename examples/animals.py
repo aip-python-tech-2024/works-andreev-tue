ce499d395f76cdcc518a281970fbbb8018b2410b
Nikolay Andreev <bakasaru@list.ru>
2024-12-24 18:31:49 +0300

    feat: add inheritance and association examples

--- examples/animals.py
+++ examples/animals.py
@@ -1,3 +1,33 @@
+# Родительский класс
+class Mammal:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Mammal(name={self.name})'
+
+    def sound(self):
+        print('...')
+
+    def eat(self):
+        print(f'{self.name} eats')
+        self.sound()
+
+
+# Дочерний класс
+class Monkey(Mammal):
+    def __init__(self, name, species):
+        # super() == self as parent class (Mammal)
+        super().__init__(name)
+        self.species = species
+
+    def __repr__(self):
+        return f'Monkey(name={self.name}, species={self.species})'
+
+    def sound(self):
+        print('Hoo hoo hah!')
+
+
 class Dog:
     def greet(self):
         print('Bark Bark!')
@@ -12,6 +42,16 @@ class Person:
     def greet(self):
         print('Hello!')
 
+mammal = Mammal('Бабуин')
+print(mammal.name)
+print(mammal)
+mammal.eat()
+
+monkey = Monkey('Абу', 'Шимпанзе')
+print(monkey.name)
+print(monkey)
+monkey.sound()
+monkey.eat()
 
 finn = Dog()
 murr = Cat()
--- examples/banks.py
+++ examples/banks.py
@@ -1,10 +1,24 @@
-class Client:
-    def __init__(self, name, initial_balance):
-        self.__name = name
-        self.__balance = initial_balance
+from datetime import datetime
+
+
+class PassportInfo:
+    def __init__(self, passport_serial, passport_number, issued_at):
+        self.__passport_serial = passport_serial
+        self.__passport_number = passport_number
+        self.__issued_at = issued_at
 
     def __repr__(self):
-        return f'<Client {self.__name}, balance: {self.__balance}>'
+        return f'{self.__passport_serial} {self.__passport_number} ({self.__issued_at})'
+
+
+class Client:
+    def __init__(self, name, passport_info, initial_balance):
+        self.__name = name
+        self._passport_info = passport_info
+        self._balance = initial_balance
+
+    def __repr__(self):
+        return f'<Client {self.__name}, balance: {self._balance}, passport: {self._passport_info}>'
 
     # Свойство с геттером и сеттером
     @property
@@ -41,22 +55,44 @@ class Client:
         self.__name = name
 
     def get_balance(self):
-        return self.__balance
+        return self._balance
 
     def send_to(self, receiver, amount):
-        if amount > self.__balance:
+        if amount > self._balance:
             # В идеале выкинуть исключение
             return False
 
         if not isinstance(receiver, Client):
             return False
 
-        self.__balance -= amount
-        receiver.__balance += amount
+        self._balance -= amount
+        receiver._balance += amount
+
+    def pay_for_maintenance(self):
+        amount = 10
+        if amount > self._balance:
+            print('Not enough balance for account maintenance')
+            return False
+        self._balance -= amount
 
 
-me = Client('Bakasa', 100)
-friend = Client('Dewili', 200)
+class VipClient(Client):
+    def __init__(self, name, passport_info, initial_balance):
+        super().__init__(name, passport_info, initial_balance)
+
+    def pay_for_maintenance(self):
+        amount = 5
+        if amount > self._balance:
+            print('Not enough balance for account maintenance')
+            return False
+        self._balance -= amount
+
+
+my_passport = PassportInfo('1234', '567890', datetime.now())
+me = Client('Bakasa', my_passport, 100)
+
+friends_passport = PassportInfo('9876', '543210', datetime.now())
+friend = VipClient('Dewili', friends_passport, 200)
 print(me, friend)
 
 me.send_to(friend, 150)
@@ -83,3 +119,12 @@ friend.name += ' Andreev'
 
 print(me.name)
 print(friend.name)
+
+me.pay_for_maintenance()
+friend.pay_for_maintenance()
+
+print(me)
+print(friend)
+
+me._balance += 10000
+print(me)
